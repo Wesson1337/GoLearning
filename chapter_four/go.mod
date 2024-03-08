230205674d1cb1543a3db2200cd175f2358ae856
Wesson1337 <vinokurov.filipp1@yandex.ru>
2024-03-08 04:18:53 +0300

    chapter four finished

---
+++ chapter_four/go.mod
@@ -0,0 +1,3 @@
+module chapter_four
+
+go 1.22.0
---
+++ chapter_four/issesreport.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+	"time"
+	"chapter_four/github"
+)
+
+const temp = `{{.TotalCount}} issues:
+{{range .Items}}----------------------------------------
+Number: {{.Number}}
+User: {{.User.Login}}
+Title: {{.Title | printf "%.64s"}}
+Age: {{.CreatedAt | daysAgo}} days
+{{end}}`
+
+func daysAgo(t time.Time) int {
+	return int(time.Since(t).Hours() / 24)
+}
+
+var report = template.Must(template.New("issuelist").
+	Funcs(template.FuncMap{"daysAgo": daysAgo}).
+	Parse(temp))
+
+func main() {
+	result, err := github.SearchIssues([]string{"repo:golang/go", "is:open", "json", "decoder"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
+}
