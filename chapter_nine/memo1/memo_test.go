cea422bf99572cb55f4d8093639f8a315c983f1c
Wesson1337 <vinokurov.filipp1@yandex.ru>
2024-04-16 05:37:50 +0300

    chapter_nine

---
+++ chapter_nine/memo1/memo_test.go
@@ -0,0 +1,20 @@
+package memo_test
+
+import (
+	memo "golearning/chapter_nine/memo1"
+	"golearning/chapter_nine/memotest"
+	"testing"
+)
+
+var httpGetBody = memotest.HTTPGetBody
+
+func Test(t *testing.T) {
+	m := memo.New(httpGetBody)
+	memotest.Sequential(t, m)
+}
+
+// NOTE: not concurrency-safe!  Test fails.
+func TestConcurrent(t *testing.T) {
+	m := memo.New(httpGetBody)
+	memotest.Concurrent(t, m)
+}
--- chapter_nine/test/main.go
+++ chapter_nine/test/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
 	"sync"
 	"time"
 )
@@ -19,5 +21,48 @@ func main() {
 	}()
 	time.Sleep(1 * time.Second)
 
-	sync.Mutex
+	var mp map[string]int
+	res := mp["a"]
+	fmt.Println(res)
+
+	for i := 0; i < 100; i++ {
+		go fmt.Print(0)
+		fmt.Print(1)
+	}
+
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+
+	mu := sync.Mutex{}
+	i := 0
+
+	go func() {
+		ticker := time.Tick(1 * time.Second)
+		for {
+			select {
+			case <-ticker:
+				mu.Lock()
+				fmt.Println("i:", i)
+				mu.Unlock()
+			default:
+				msg := <-ch1
+				ch2 <- msg
+				mu.Lock()
+				i++
+				mu.Unlock()
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			ch1 <- "hello"
+			<-ch2
+		}
+	}()
+
+	<-sigCh
+
 }
